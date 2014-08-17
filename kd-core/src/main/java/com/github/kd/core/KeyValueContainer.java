632a1806ed05fd9774d3c2980dc0b57a774bfb55
Ruslan Diachenko <rd.ryly@gmail.com>
2014-08-17 22:24:51 +0300

    #11: kd basic structures were added

---
+++ kd-core/src/main/java/com/github/kd/core/KeyValueContainer.java
@@ -0,0 +1,5 @@
+package com.github.kd.core;
+
+public class KeyValueContainer {
+
+}
---
+++ kd-core/src/main/java/com/github/kd/core/Node.java
@@ -0,0 +1,10 @@
+package com.github.kd.core;
+
+public class Node {
+
+	private byte[] id;
+
+	private KeyValueContainer keyValueContainer;
+
+	private RoutingTable routingTable;
+}
